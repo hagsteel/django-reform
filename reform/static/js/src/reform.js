61f7f9794d38a7f939118e9d8fbdbafbf74a0be5
Jonas Hagstedt <hagstedt@gmail.com>
2015-02-27 23:54:01 +0100

    Expose form manager

--- reform/static/js/src/reform.js
+++ reform/static/js/src/reform.js
@@ -1,10 +1,12 @@
 var ReForm = require('./components/form');
 var TemplateManager = require('./template-manager');
 var ErrorList = require('./components/widgets/templates/base_error_list');
+var FormManager = require('./form-manager');
 
 
 module.exports = {
     ReForm: ReForm,
     TemplateManager: TemplateManager,
-    ErrorList: ErrorList
+    ErrorList: ErrorList,
+    FormManager: FormManager
 };
